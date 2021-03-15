4d17b60a9613f887d446ca36d3c4bdf7ea9a1b48
Chris Knott <s206362@uos.ac.uk>
2021-03-15 20:17:36 +0000

    Saving current status
    Error
    1) JSON is working correctly for both program and actor search
    2) However spinning wheel is appearing all the time. - need help sorting it out.

--- tv-app/src/screens/Actors.js
+++ tv-app/src/screens/Actors.js
@@ -1,16 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
 export default function ActorsScreen({ navigation }) {
-    return (
-        <View style={styles.ActorsScreen}>
-            <Text>List of TV Actors Screen</Text>
-        </View>
+  const [searchQuery, setSearchQuery] = useState("Smith"); // step 5??????  numbering error in activity   ??????????????????????????
+  const [actors, setActors] = useState(); // activity numbering errors - is this in correct place?   ??????????????????????????????
+
+  const searchActors = () => {
+    console.log(
+      "Make a call to the API using the search query: " + searchQuery
     );
+    fetch("http://api.tvmaze.com/search/people?q=" + searchQuery)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setActors(json["results"]); // is this correct positioning?????????????????????
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  useEffect(() => {
+    searchActors();
+  }, [searchQuery]);
+
+  return (
+    <View style={styles.ActorsScreen}>
+      {actors ? (
+        <View style={styles.resultsContainer}>
+          <FlatList
+            data={actors}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+          />
+        </View>
+      ) : (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#000" />
+        </View>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    ActorsScreen: {
-        /* styles here */
-    },
+  ActorsScreen: {
+    /* styles here */
+  },
+  loadingContainer: {
+    /* styles here */
+    height: "100%",
+    justifyContent: "center",
+  },
 });
--- tv-app/src/screens/Progs.js
+++ tv-app/src/screens/Progs.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ActivityIndicator, FlatList } from "react-native";
 
 export default function ProgsScreen({ navigation }) {
-    return (
-        <View style={styles.ProgsScreen}>
-            <Text>List of TV Programs Screen</Text>
-        </View>
+  const [searchQuery, setSearchQuery] = useState("News");
+  const [progs, setProgs] = useState(); // //////////////////////////Is this positioned correctly???????? penultimate step from activity 20 ??????????
+
+  const searchProgs = () => {
+    console.log(
+      "Make a call to the API using the search query: " + searchQuery
     );
+    fetch("http://api.tvmaze.com/search/shows?q=" + "?=" + searchQuery)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setProgs(json["results"]); // //////////////////////////Is this positioned correctly???????? last step activity 20  ?????????????????????
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    searchProgs();
+  }, [searchQuery]);
+
+  return (
+    <View style={styles.ProgsScreen}>
+      {progs ? (
+        <View style={styles.resultsContainer}>
+          <FlatList
+            data={progs}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+          />
+        </View>
+      ) : (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#000" />
+        </View>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    ProgsScreen: {
-        /* styles here */
-    },
+  ProgsScreen: {
+    /* styles here */
+  },
+
+  loadingContainer: {
+    height: "100%",
+    justifyContent: "center",
+  },
 });
