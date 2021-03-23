36d001a37e41db0ed1b53a089638d87028faefc5
Chris Knott <s206362@uos.ac.uk>
2021-03-23 11:11:13 +0000

    TV after activity 23
    Detail screens are working with two issues:
    missing photo images causes failure
    
    writeup includes <p> tags etc,

--- tv-app/src/screens/ActorDetails.js
+++ tv-app/src/screens/ActorDetails.js
@@ -1,13 +1,42 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { global } from "../config/global"; //////
 export default function ActorDetailsScreen({ route, navigation }) {
+  const [actorData, setActorData] = useState(); ///////
+
   const { photoId } = route.params;
   console.log("show details for photo: " + photoId);
+  const getActorData = () => {
+    fetch("http://api.tvmaze.com/people/" + photoId)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setActorData(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getActorData();
+  }, [photoId]);
 
   return (
     <View style={styles.ActorDetailsScreen}>
-      <Text>ActorDetails screen</Text>
+      {actorData ? (
+        <View style={styles.detailsContainer}>
+          <Text>{actorData.name}</Text>
+          <Text>{actorData.birthday}</Text>
+          <Text>{actorData.deathday}</Text>
+          <Text>{actorData.gender}</Text>
+          <Text>{actorData.image.original}</Text>
+        </View>
+      ) : (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#000" />
+        </View>
+      )}
     </View>
   );
 }
@@ -15,5 +44,10 @@ export default function ActorDetailsScreen({ route, navigation }) {
 const styles = StyleSheet.create({
   ActorDetailsScreen: {
     /* styles here */
+  },
+
+  loadingContainer: {
+    height: "100%",
+    justifyContent: "center",
   },
 });
--- tv-app/src/screens/ProgDetails.js
+++ tv-app/src/screens/ProgDetails.js
@@ -1,13 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { global } from "../config/global"; //////
 export default function ProgDetailsScreen({ route, navigation }) {
+  const [progData, setProgData] = useState(); ///////
   const { photoId } = route.params;
   console.log("show details for photo: " + photoId);
+  const getProgData = () => {
+    fetch("http://api.tvmaze.com/shows/" + photoId)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setProgData(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getProgData();
+  }, [photoId]);
 
   return (
     <View style={styles.ProgDetailsScreen}>
-      <Text>ProgDetails screen</Text>
+      {progData ? (
+        <View style={styles.detailsContainer}>
+           <Text>{progData.name}</Text>    
+          <Text>{progData.language}</Text>
+          <Text>{progData.summary}</Text>
+          <Text>{progData.premiered}</Text>
+          <Text>{progData.image.original}</Text>   
+        </View>
+      ) : (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#000" />
+        </View>
+      )}
     </View>
   );
 }
@@ -15,5 +43,9 @@ export default function ProgDetailsScreen({ route, navigation }) {
 const styles = StyleSheet.create({
   ProgDetailsScreen: {
     /* styles here */
+  },
+  loadingContainer: {
+    height: "100%",
+    justifyContent: "center",
   },
 });
